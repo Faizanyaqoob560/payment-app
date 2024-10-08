158a71d9982970c279b2bc3392442138fb8b8e48
Faizanyaqoob560 <faizanyaqoob347@gmail.com>
2024-10-08 11:24:21 +0500

    updated profile screen

--- flutter_application_1/lib/profile_screen.dart
+++ flutter_application_1/lib/profile_screen.dart
@@ -1,7 +1,20 @@
+import 'dart:js_interop';
+
 import 'package:flutter/material.dart';
 
-class ProfileScreen extends StatelessWidget {
+class ProfileScreen extends StatefulWidget {
   const ProfileScreen({super.key});
+
+  @override
+  State<ProfileScreen> createState() => _ProfileScreenState();
+}
+
+class _ProfileScreenState extends State<ProfileScreen> {
+  String userName = "Faizan Yaqoob";
+  String UserEmail = "faizanyaqoob347@gmail.com";
+  String ProfileImage = "  Image";
+
+  //CREATING function Method for profile button,
 
   @override
   Widget build(BuildContext context) {
@@ -18,6 +31,30 @@ class ProfileScreen extends StatelessWidget {
               radius: 60,
               backgroundImage: NetworkImage("url"),
             ),
+            const SizedBox(
+              height: 20,
+            ),
+            Text(
+              "user name",
+              style: TextStyle(
+                fontSize: 16,
+                color: Colors.grey,
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Text(
+              "user Email",
+              style: TextStyle(
+                fontSize: 16,
+                color: Colors.grey,
+              ),
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            //EDIT PROFILE BUTTON
           ],
         ),
       ),
